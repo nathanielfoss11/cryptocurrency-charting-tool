d2c4aa4b8face66a6b4f9d55fdfe834e42617fb4
Nate Foss <nathanielfoss11@gmail.com>
2021-01-06 21:45:47 -0700

    Initial repo setup

---
+++ client/src/CryptoApp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class CryptoApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Cryptocurrency Charting Tool</h1>
+      </div>
+    );
+  }
+}
+
+export default CryptoApp;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import CryptoApp from './CryptoApp.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<CryptoApp/>, document.getElementById('root'));
